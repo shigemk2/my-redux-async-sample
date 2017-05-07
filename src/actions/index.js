79537088a8dcf34d16e0b4d17aaf32b4b1e071bc
shigemk2 <i.am.shige@gmail.com>
2017-05-07 17:58:43 +0900

    actions/index

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,29 +1,31 @@
-export const REQUEST_POSTS = 'REQUEST_POSTS'
-export const RECEIVE_POSTS = 'RECEIVE_POSTS'
-export const SELECT_REDDIT = 'SELECT_REDDIT'
-export const INVALIDATE_REDDIT = 'INVALIDATE_REDDIT'
+export const REQUEST_POSTS = 'REQUEST_POSTS';
+export const RECEIVE_POSTS = 'RECEIVE_POSTS';
+export const SELECT_REDDIT = 'SELECT_REDDIT';
+export const INVALIDATE_REDDIT = 'INVALIDATE_REDDIT';
 
+// 外で使える
 export const selectReddit = reddit => ({
   type: SELECT_REDDIT,
   reddit
-})
+});
 
+// 外で使える
 export const invalidateReddit = reddit => ({
   type: INVALIDATE_REDDIT,
   reddit
-})
+});
 
 export const requestPosts = reddit => ({
   type: REQUEST_POSTS,
   reddit
-})
+});
 
 export const receivePosts = (reddit, json) => ({
   type: RECEIVE_POSTS,
   reddit,
   posts: json.data.children.map(child => child.data),
   receivedAt: Date.now()
-})
+});
 
 // 外部URLからJSONデータを取り出す
 const fetchPosts = reddit => dispatch => {
@@ -31,7 +33,7 @@ const fetchPosts = reddit => dispatch => {
   return fetch(`https://www.reddit.com/r/${reddit}.json`)
     .then(response => response.json())
     .then(json => dispatch(receivePosts(reddit, json)))
-}
+};
 
 const shouldFetchPosts = (state, reddit) => {
   const posts = state.postsByReddit[reddit]
