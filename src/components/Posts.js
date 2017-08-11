abd7eaf72e6b5721e5121a5eccbaf956e098f80c
Michihito Shigemura <i.am.shige@gmail.com>
2017-08-12 01:17:22 +0900

    Fix

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -4,13 +4,13 @@ import PropTypes from 'prop-types'
 const Posts = ({posts}) => (
   <ul>
     {posts.map((post, i) =>
-      <li key={i}>{post}</li>
+      <li key={i}>{post.name}</li>
     )}
   </ul>
-)
+);
 
 Posts.propTypes = {
   posts: PropTypes.array.isRequired
-}
+};
 
 export default Posts
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import { selectBank, fetchPostsIfNeeded, invalidateBank } from '../actions'
-import Picker from '../components/Picker'
+import { fetchPostsIfNeeded } from '../actions'
 import Posts from '../components/Posts'
 
 // Componentクラス
 class App extends Component {
   static propTypes = {
-    selectedBank: PropTypes.string.isRequired,
     posts: PropTypes.array.isRequired,
     isFetching: PropTypes.bool.isRequired,
     lastUpdated: PropTypes.number,
@@ -27,28 +25,19 @@ class App extends Component {
     }
   }
 
-  handleChange = nextBank => {
-    this.props.dispatch(selectBank(nextBank))
-  };
-
   handleRefreshClick = e => {
     e.preventDefault();
 
     const { dispatch, selectedBank } = this.props;
-    dispatch(invalidateBank(selectedBank));
     dispatch(fetchPostsIfNeeded(selectedBank))
   };
 
   // 描画
   render() {
-    const { selectedBank, posts, isFetching, lastUpdated } = this.props;
+    const { posts, isFetching, lastUpdated } = this.props;
     const isEmpty = posts.length === 0;
-    console.log(posts.map((post, i) => console.log(post)));
     return (
       <div>
-        <Picker value={selectedBank}
-                onChange={this.handleChange}
-                options={[ 'reactjs', 'frontend' ]} />
         <p>
           {lastUpdated &&
             <span>
